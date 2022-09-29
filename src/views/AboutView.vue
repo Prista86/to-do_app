b0cd3b503ffc5d3a54c667b1db9ce92164afe7e6
Prista86 <101596856+Prista86@users.noreply.github.com>
2022-09-29 21:52:06 +0200

    Todo.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-12">
+    <h1>Vedere se fare la pagina e scrivere roba...</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit facilisis diam ac molestie. Donec quis velit et lectus gravida maximus a a ipsum. Sed at purus vitae mauris tincidunt posuere. Aliquam sit amet ipsum rutrum, hendrerit enim eu, tempor leo. Quisque pellentesque fermentum finibus. Quisque gravida ante eu tellus vestibulum efficitur. Vivamus placerat elit nec commodo sodales.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit facilisis diam ac molestie. Donec quis velit et lectus gravida maximus a a ipsum. Sed at purus vitae mauris tincidunt posuere. Aliquam sit amet ipsum rutrum, hendrerit enim eu, tempor leo. Quisque pellentesque fermentum finibus. Quisque gravida ante eu tellus vestibulum efficitur. Vivamus placerat elit nec commodo sodales.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit facilisis diam ac molestie. Donec quis velit et lectus gravida maximus a a ipsum. Sed at purus vitae mauris tincidunt posuere. Aliquam sit amet ipsum rutrum, hendrerit enim eu, tempor leo. Quisque pellentesque fermentum finibus. Quisque gravida ante eu tellus vestibulum efficitur. Vivamus placerat elit nec commodo sodales.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit facilisis diam ac molestie. Donec quis velit et lectus gravida maximus a a ipsum. Sed at purus vitae mauris tincidunt posuere. Aliquam sit amet ipsum rutrum, hendrerit enim eu, tempor leo. Quisque pellentesque fermentum finibus. Quisque gravida ante eu tellus vestibulum efficitur. Vivamus placerat elit nec commodo sodales.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit facilisis diam ac molestie. Donec quis velit et lectus gravida maximus a a ipsum. Sed at purus vitae mauris tincidunt posuere. Aliquam sit amet ipsum rutrum, hendrerit enim eu, tempor leo. Quisque pellentesque fermentum finibus. Quisque gravida ante eu tellus vestibulum efficitur. Vivamus placerat elit nec commodo sodales.
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <v-list
+      flat
+    >
+          <v-list-item
+            v-for="task in tasks"
+            :key="task.id"
+          >
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{task.title}}</v-list-item-title>              
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+
+             
+    </v-list>  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data(){
+      return{
+      tasks: [
+        {
+          id:1,
+          title: 'Wake Up1'
+        },
+        {
+          id:2,
+          title: 'Wake Up2'
+        },
+        {
+          id:3,
+          title: 'Wake Up3'
+        },
+      ]
+      }
+  }
+</script>
